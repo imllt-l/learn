f1697ecab06ca878471d4e70553df398899c8e4d
imllt <a873570845@gmail.com>
2018-05-08 22:13:26 +0800

    更新了最后一页的显示

--- app/src/main/java/com/example/imllt/learn/detailActivity.java
+++ app/src/main/java/com/example/imllt/learn/detailActivity.java
@@ -2,49 +2,75 @@ package com.example.imllt.learn;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.baidu.location.LocationClient;
-import com.baidu.mapapi.map.TextureMapView;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
 
 public class detailActivity extends AppCompatActivity {
 
     public static final String POSITION_NAME ="position_name";
     public static final String POSITION_IMAGE_ID ="position_image_id";
     public static final String POSITION_TEXT ="position_text";
-    private TextureMapView mMapView;
-    public LocationClient mLocationClient;
-    private boolean isFirstLocate = true;
-    private MapView mapView;
-    private ImageView positionmap;
+
+    private enum CollapsingToolbarLayoutState {
+        EXPANDED,
+        COLLAPSED,
+        INTERNEDIATE
+    }
+    CollapsingToolbarLayoutState state;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mLocationClient = new LocationClient(getApplicationContext());
         setContentView(R.layout.activity_detail);
         //初始化
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.colltollBar);
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.colltollBar);
         ImageView imageView = (ImageView)findViewById(R.id.toolBar_Iamageview);
-
         TextView textView =(TextView)findViewById(R.id.map_Imageview);
 
-        positionmap = (ImageView) findViewById(R.id.position_map);
-        /////
-        Mapbox.getInstance(this, getString(R.string.access_token));
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+
+        //对上方的AppBar进行监听
+        AppBarLayout appBar = (AppBarLayout)findViewById(R.id.appBar);
+        appBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                ButtonBarLayout barText = (ButtonBarLayout) findViewById(R.id.barText);
+                if(verticalOffset == 0){
+                    if(state != CollapsingToolbarLayoutState.EXPANDED){
+                        state = CollapsingToolbarLayoutState.EXPANDED;
+                        collapsingToolbarLayout.setTitle("EXPANDED");
+                    }
+                }
+                else if(Math.abs(verticalOffset)>=appBarLayout.getTotalScrollRange()){
+                    if(state != CollapsingToolbarLayoutState.COLLAPSED){
+                        collapsingToolbarLayout.setTitle("");
+                        barText.setVisibility(View.VISIBLE);
+                        state = CollapsingToolbarLayoutState.COLLAPSED;
+                    }
+                }
+                else {
+                    if (state != CollapsingToolbarLayoutState.INTERNEDIATE) {
+                        if(state == CollapsingToolbarLayoutState.COLLAPSED){
+                            barText.setVisibility(View.GONE);
+                        }
+                        collapsingToolbarLayout.setTitle("INTERNEDIATE");//设置title为INTERNEDIATE
+                        state = CollapsingToolbarLayoutState.INTERNEDIATE;//修改状态标记为中间
+                    }
+                }
+            }
+        });
 
         Intent intent = getIntent();
         String positionname = intent.getStringExtra(POSITION_NAME);
         int positionimageid = intent.getIntExtra(POSITION_IMAGE_ID,0);
         String position_card_text = intent.getStringExtra(POSITION_TEXT);
+
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayShowHomeEnabled(true);
@@ -52,7 +78,6 @@ public class detailActivity extends AppCompatActivity {
         collapsingToolbarLayout.setTitle(positionname);
         collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.cardview_light_background));
         collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.colorAccent));
-        //Glide.with(this).load(positionimageid).into(imageView);
         imageView.setImageResource(positionimageid);
         textView.setText(position_card_text);
     }
